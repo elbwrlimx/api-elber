5c1011d5e8c1f2e71ec0013959a2429ce94d241d
elbwrlimx <elberlima44@gmail.com>
2025-05-13 16:02:37 -0300

    new project :)

---
+++ api/src/test/java/first/elber/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package first.elber.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

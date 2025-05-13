1b3d87d27adc3e249debc58a54d8508894a222aa
elbwrlimx <elberlima44@gmail.com>
2025-05-13 16:24:01 -0300

    atts

---
+++ api/src/main/java/first/elber/api/controller/helloController.java
@@ -0,0 +1,16 @@
+package first.elber.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class helloController {
+
+    @GetMapping
+    public String oiiString() {
+        return "Hello World";
+    }
+
+}
